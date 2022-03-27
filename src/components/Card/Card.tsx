c0a47f920fe9b18fef8c46492880a4e73d1853b0
Michael Jun-Dundas <umjundun@gmail.com>
2022-03-27 18:43:21 -0500

    added cards, with css

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,40 @@
 import React from "react";
+// import styles from "./Card.module.css";
 import "./Card.css";
 
 interface CardProps {
-    label: string;
+    image: string;
+    text_column: string;
 }
 
 const Card = (props: CardProps) => {
     return (
-        <div>
-            <img src="https://cdn.rapidrtc.com/wp-content/uploads/2021/03/BuyerJourney-Icon-1.png" alt=""/>
-        </div>
+        <div className="vc_column-inner vc_custom_1616791165196">
+            <div className="wpb_wrapper">
+                <div className="vc_empty_space" style={{height: 32}}>
+                <span className="vc_empty_space_inner"></span>
+                </div>
+	<div className="wpb_single_image wpb_content_element vc_align_center   support-logo">
+		
+		<figure className="wpb_wrapper vc_figure">
+			<div className="vc_single_image-wrapper   vc_box_border_grey"><img width="196" height="72" src={props.image} className="vc_single_image-img attachment-medium" alt="" loading="lazy" /></div>
+		</figure>
+	</div>
+<div className="vc_empty_space" style={{height: 32}}><span className="vc_empty_space_inner"></span></div>
+	<div className="wpb_text_column wpb_content_element  vc_custom_1561411385522 text-center">
+		<div className="wpb_wrapper">
+			<p>{props.text_column}</p>
+
+		</div>
+	</div>
+<div className="vc_empty_space" style={{height: 32}}><span className="vc_empty_space_inner"></span></div>
+	<div className="wpb_text_column wpb_content_element  text-center">
+		<div className="wpb_wrapper">
+			<p><a className="see-what-btn see-what" href="/">LEARN MORE</a></p>
+
+		</div>
+	</div>
+<div className="vc_empty_space" style={{height: 60}}><span className="vc_empty_space_inner"></span></div></div></div>
     );
 };
 
--- src/stories/testing.stories.js
+++ src/stories/testing.stories.js
@@ -8,11 +8,12 @@ const stories = storiesOf("app test", module);
 
 stories.add("App", () => {
     return (
-        <div>
+    <div>
         <Button label="Button Test"
         />
         <br />
-        <Card title="Card Test" />
-        </div>
+        <Card image="https://cdn.rapidrtc.com/wp-content/uploads/2021/03/Training-Logo-1.png" text_column="You and your dealership move fast, so you need to learn fast, and we get that."/>
+        <Card image="https://cdn.rapidrtc.com/wp-content/uploads/2021/03/DealerCare-Logo-1.png" text_column="When it comes to service, you want to talk to someone and we want to talk to you." />
+    </div>
     );
 });
